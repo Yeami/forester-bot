78f361d49fac9b935fdb1d165062828382c9c87d
Yeami <ivanov.nikita@yeami.group>
2020-06-21 14:37:19 +0300

    Implement the very basic bot

---
+++ bot.py
@@ -0,0 +1,15 @@
+import os
+
+import discord
+
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='/')
+
+
+@client.event
+async def on_ready():
+    print('I am ready!')
+
+
+client.run(os.environ['BOT_TOKEN'])
