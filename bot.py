94bf38a63b14877d365225d64e4768f6e16aa0f1
Yeami <ivanov.nikita@yeami.group>
2020-06-21 21:25:20 +0300

    Implement the unban command and add the channel notification for the ban command

--- bot.py
+++ bot.py
@@ -47,6 +47,21 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 @client.command()
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
+    await ctx.send(f'The user {member.mention} was banned!')
+
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for entry in banned_users:
+        user = entry.user
+
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'The user {user.mention} was unbanned!')
+            return
 
 
 client.run(os.environ['BOT_TOKEN'])
