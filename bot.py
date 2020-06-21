06338df9e8ed5983a6cfb73e3f92310db999b30b
Yeami <ivanov.nikita@yeami.group>
2020-06-21 14:51:12 +0300

    Implement the basic handlers for members join/left actions on the server

--- bot.py
+++ bot.py
@@ -12,4 +12,14 @@ async def on_ready():
     print('I am ready!')
 
 
+@client.event
+async def on_member_join(member):
+    print(f'The {member} has joined to the server!')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'The {member} has left the server!')
+
+
 client.run(os.environ['BOT_TOKEN'])
